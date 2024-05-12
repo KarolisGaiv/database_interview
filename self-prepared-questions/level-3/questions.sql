9b0bb4209acfe91c7d7efaaffd118f1e8e8414ae
KarolisG <kgaivenis@gmail.com>
2024-05-12 21:04:08 +0300

    add lvl3 self-prepared question

--- self-prepared-questions/level-3/questions.sql
+++ self-prepared-questions/level-3/questions.sql
@@ -1,10 +1,8 @@
--- Write a query that identifies individuals who have both directed and starred in movies, but not necessarily the same ones. List their names and the titles of movies they directed and those they acted in, respectively.
+-- List all movies and any associated director names, ensuring that movies without a director are also included. Provide the movie title, year, and the director's name. Movies should be ordered by release year in descending order.
 
-
-SELECT DISTINCT name, movies.title
-    FROM people
-    JOIN directors ON directors.person_id = people.id
-    JOIN stars ON stars.person_id = people.id
-    JOIN movies ON movies.id = directors.movie_id
-    WHERE stars.person_id = people.id
-    AND directors.person_id = people.id;
+SELECT movies.title, movies.year, people.name 
+    FROM movies
+    LEFT JOIN directors ON directors.movie_id = movies.id
+    LEFT JOIN people ON directors.person_id = people.id
+    ORDER BY movies.year DESC
+    LIMIT 3000;
