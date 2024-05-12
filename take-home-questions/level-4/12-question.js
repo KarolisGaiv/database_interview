f7297cddbfd6b33b2ccc92b8f5024d56b237064d
KarolisG <kgaivenis@gmail.com>
2024-05-12 17:34:11 +0300

    delete empty file

--- take-home-questions/level-4/12-question.js
+++
@@ -1,2 +0,0 @@
-// Write a Node.js script that takes a movie title and year as a command-line argument. Print an error message if a movie with the same title and year already exists in the database. If it does not exist, add a new row for the movie and its rating (null rating, 0 votes) to the database. If any query fails, roll back your changes. If queries succeed, print the new movie ID to the console. Use query parameters.
-
