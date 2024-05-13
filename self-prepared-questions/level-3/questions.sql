d923773b92d40ee3cb19af2d2f9f594141ecf494
KarolisG <kgaivenis@gmail.com>
2024-05-13 08:12:51 +0300

    add lvl3 self-prepared question

--- self-prepared-questions/level-3/questions.sql
+++ self-prepared-questions/level-3/questions.sql
@@ -1,4 +1,4 @@
--- List all movies and any associated director names, ensuring that movies without a director are also included. Provide the movie title, year, and the director's name. Movies should be ordered by release year in descending order.
+-- 1. List all movies and any associated director names, ensuring that movies without a director are also included. Provide the movie title, year, and the director's name. Movies should be ordered by release year in descending order.
 
 SELECT movies.title, movies.year, people.name 
     FROM movies
@@ -6,3 +6,11 @@ SELECT movies.title, movies.year, people.name
     LEFT JOIN people ON directors.person_id = people.id
     ORDER BY movies.year DESC
     LIMIT 3000;
+
+-- 2. List movies that have no recorded ratings and were released after 2010. 
+
+SELECT title 
+    FROM movies
+    LEFT JOIN ratings ON ratings.movie_id = movies.id
+    WHERE year > 2010
+    AND ratings.rating IS NULL;
